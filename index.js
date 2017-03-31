d9fa9c846f41604e16f85756fd2434b07540c195
az <1457800393@qq.com>
2017-03-31 16:00:57 +0800

    121

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',function(){
+	$('#but')[0].addEventListener('touchstart',function(){
+		for(var i=0;i<100;i++){
+			if(i==0)alert('欢迎你进入猪的世界');
+			if(i==0)alert('欢迎你进入猪的世界');
+		};
+	});
+},false);
